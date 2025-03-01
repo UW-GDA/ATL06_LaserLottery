e7d6db573b3119c2d2b27f59ea714af447300f4f
Jack-Hayes <jehayes@uw.edu>
2025-02-28 16:17:26 -0800

    nb 01 draft

---
+++ misc/nlcd_plot.py
@@ -0,0 +1,13 @@
+nlcd_colors = {
+    11: '#526079', 12: '#9CA1B6', 21: '#D99282', 22: '#EB8F6A', 23: '#ED0000',
+    24: '#AA0000', 31: '#B3AFA4', 41: '#68AB63', 42: '#1C6330', 43: '#B5C98E',
+    51: '#CCB879', 52: '#CCBA7C', 71: '#DFD355', 72: '#AA7028', 73: '#BAA065',
+    74: '#D1BB80', 81: '#DDD83E', 82: '#AE7229', 90: '#87D19E', 95: '#71A4C1'
+}
+nlcd_labels = {
+    11: "Open Water", 12: "Perennial Ice/Snow", 21: "Developed, Open Space", 22: "Developed, Low Intensity",
+    23: "Developed, Medium Intensity", 24: "Developed, High Intensity", 31: "Barren Land", 41: "Deciduous Forest",
+    42: "Evergreen Forest", 43: "Mixed Forest", 51: "Dwarf Scrub", 52: "Shrub/Scrub", 71: "Grassland/Herbaceous",
+    72: "Sedge/Herbaceous", 73: "Lichens", 74: "Moss", 81: "Pasture/Hay", 82: "Cultivated Crops", 90: "Woody Wetlands",
+    95: "Emergent Herbaceous Wetlands"
+}
